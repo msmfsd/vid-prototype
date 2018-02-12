09d21a96dd1b65c2a3a3a32d94a4ad34f1b58507
Magnus Morris <magnus@mydr.com.au>
2018-02-12 17:15:49 +1100

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,229 @@
+import React, { Component } from 'react';
+import styled from 'styled-components'
+import TwilioVideo from 'twilio-video'
+
+/* *** LIB **/
+// Attach the Tracks to the DOM.
+const attachTracks = (tracks, container) => {
+  tracks.forEach((track) => {
+    container.appendChild(track.attach());
+  });
+}
+
+// Attach the Participant's Tracks to the DOM.
+const attachParticipantTracks = (participant, container) => {
+  let tracks = Array.from(participant.tracks.values());
+  attachTracks(tracks, container);
+}
+
+// Detach the Tracks from the DOM.
+const detachTracks = (tracks) => {
+  tracks.forEach(function(track) {
+    track.detach().forEach(function(detachedElement) {
+      detachedElement.remove();
+    });
+  });
+}
+
+// Detach the Participant's Tracks from the DOM.
+const detachParticipantTracks = (participant) => {
+  var tracks = Array.from(participant.tracks.values());
+  detachTracks(tracks);
+}
+/* **** */
+
+const Main = styled.div`
+  display: flex;
+  position: relative;
+  width: 80%;
+  height: auto;
+  margin: 20px auto;
+  padding: 20px;
+  background-color: #444;
+  color: #dfdfdf;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Button = styled.button`
+  background-color: #fff;
+  color: #444;
+  font-size: 18px;
+  margin-right: 5px;
+  cursor: pointer;
+  &:disabled {
+    cursor: default;
+    opacity: 0.4;
+  }
+`;
+
+const Videos = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  width: 100%;
+  height: auto;
+  margin-bottom: 20px;
+`;
+
+const Video = styled.div`
+  min-height: 200px;
+  width: 50%;
+  & video {
+    width: 100%;
+  }
+`;
+
+const InComingVideo = styled(Video)`
+  background-color: #242424;
+`;
+
+const OutGoingVideo = styled(Video)`
+  background-color: #131313;
+`;
+
+class App extends Component {
+  state = {
+    previewTracks: null,
+    activeRoom: null,
+    identity: null,
+    token: null,
+    roomName: null,
+  };
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => {
+        this.setState({
+          identity: res.identity,
+          token: res.token
+        })
+      })
+      .catch(err => console.log(err));
+  }
+
+  componentWillUnMount = () => {
+    if (this.state.activeRoom) this.state.activeRoom.disconnect();
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/token');
+    const body = await response.json();
+    if (response.status !== 200) throw Error(body.message);
+    return body;
+  }
+
+  setActiveRoom = (state) => {
+    this.setState({ activeRoom: state });
+  }
+
+  // Successfully connected!
+  roomJoined = (room) => {
+    const {previewTracks} = this.state
+
+    this.setActiveRoom(room)
+    console.log("Joined as '" + this.state.identity + "'");
+
+    // Attach LocalParticipant's Tracks, if not already attached.
+    let outgoingPreviewContainer = document.getElementById('outgoing-video');
+    if (!outgoingPreviewContainer.querySelector('video')) {
+      attachParticipantTracks(room.localParticipant, outgoingPreviewContainer);
+    }
+
+    // Attach the Tracks of the Room's Participants.
+    room.participants.forEach((participant) => {
+      console.log("Already in Room: '" + participant.identity + "'");
+      let incomingPreviewContainer = document.getElementById('incoming-video');
+      attachParticipantTracks(participant, incomingPreviewContainer);
+    });
+
+    // When a Participant joins the Room, log the event.
+    room.on('participantConnected', (participant) => {
+      console.log("Joining: '" + participant.identity + "'");
+    });
+
+    // When a Participant adds a Track, attach it to the DOM.
+    room.on('trackAdded', (track, participant) => {
+      console.log(participant.identity + " added track: " + track.kind);
+      let incomingPreviewContainer = document.getElementById('incoming-video');
+      attachTracks([track], incomingPreviewContainer);
+    });
+
+    // When a Participant removes a Track, detach it from the DOM.
+    room.on('trackRemoved', (track, participant) => {
+      console.log(participant.identity + " removed track: " + track.kind);
+      detachTracks([track]);
+    });
+
+    // When a Participant leaves the Room, detach its Tracks.
+    room.on('participantDisconnected', (participant) => {
+      console.log("Participant '" + participant.identity + "' left the room");
+      detachParticipantTracks(participant);
+    });
+
+    // Once the LocalParticipant leaves the room, detach the Tracks
+    // of all Participants, including that of the LocalParticipant.
+    var that = this;
+    room.on('disconnected', () => {
+      console.log('Left');
+      if (previewTracks) {
+        previewTracks.forEach((track) => {
+          track.stop();
+        });
+      }
+      detachParticipantTracks(room.localParticipant);
+      room.participants.forEach(detachParticipantTracks);
+      that.setActiveRoom(null)
+    });
+  }
+
+  beginConsult = (event) => {
+    event.preventDefault();
+    const {previewTracks, token} = this.state
+    const roomName = 'consultation1'
+
+    this.setState({ roomName: roomName })
+    console.log("Joining room '" + roomName + "'...");
+
+    const connectOptions = {
+      name: roomName,
+      /*logLevel: 'debug'*/
+    };
+
+    if (previewTracks) {
+      connectOptions.tracks = previewTracks;
+    }
+
+    // VideoCodecs=H264
+    console.log('TwilioVideo',TwilioVideo);
+    // Join the Room with the token from the server and the
+    // LocalParticipant's Tracks.
+    TwilioVideo.connect(token, connectOptions).then(this.roomJoined, (error) => {
+      console.log('Could not connect to Twilio: ' + error.message);
+    });
+  }
+
+  endConsult = (event) => {
+    event.preventDefault();
+    console.log('Leaving room...');
+    this.state.activeRoom.disconnect();
+  }
+
+  render() {
+    return (
+      <Main>
+        <Videos>
+          <InComingVideo id="incoming-video" />
+          <OutGoingVideo id="outgoing-video" />
+        </Videos>
+        <div>
+          <Button disabled={this.state.activeRoom !== null} onClick={this.beginConsult}>Begin consult</Button>
+          <Button disabled={this.state.activeRoom === null} onClick={this.endConsult}>End consult</Button>
+        </div>
+      </Main>
+    );
+  }
+}
+
+export default App;
---
+++ server.js
@@ -0,0 +1,45 @@
+import http from 'http';
+import path from 'path';
+import twilio from 'twilio';
+import express from 'express';
+import dotenv from 'dotenv'
+import randomName from './randomname';
+
+dotenv.load();
+
+const AccessToken = twilio.jwt.AccessToken;
+const VideoGrant = AccessToken.VideoGrant;
+const app = express();
+const port = process.env.PORT || 5000;
+
+/**
+ * Generate an Access Token for a chat application user - it generates a random
+ * username for the client requesting a token, and takes a device ID as a query
+ * parameter.
+ */
+app.get('/api/token', (request, response) => {
+  const identity = randomName();
+
+  // Create an access token which we will sign and return to the client,
+  // containing the grant we just created.
+  const token = new AccessToken(
+    process.env.TWILIO_ACCOUNT_SID,
+    process.env.TWILIO_API_KEY,
+    process.env.TWILIO_API_SECRET
+  );
+
+  // Assign the generated identity to the token.
+  token.identity = identity;
+
+  // Grant the access token Twilio Video capabilities.
+  const grant = new VideoGrant();
+  token.addGrant(grant);
+
+  // Serialize the token to a JWT string and include it in a JSON response.
+  response.send({
+    identity: identity,
+    token: token.toJwt()
+  });
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
